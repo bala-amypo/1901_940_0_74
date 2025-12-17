67b6cafea399b0ad892f0ac048edf9255068b9ce
bala-amypo <bala@amypo.in>
2025-12-17 05:14:43 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,7 +1,4 @@
-package com.example.aiml.entity;
-
-import java.time.LocalDate;
-
+package com.example.demo.entity;
 public class Student {
    private int id;
    private String name;
@@ -25,14 +22,11 @@ public class Student {
    public void setEmail(String email) {
     this.email = email;
    }
-   
-   
-   public Student(int id, String name, String email, LocalDate dob, float cgpa) {
+   public Student(int id, String name, String email) {
     this.id = id;
     this.name = name;
     this.email = email;
-    this.dob = dob;
-    this.cgpa = cgpa;
+    
    }
    public Student() {
    }
