9185fb982b0b2b6abd2f36416d203b3e9301a13a
bala-amypo <bala@amypo.in>
2025-12-17 17:07:49 +0000

    User commit

--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -10,6 +10,7 @@ import jakarta.persistence.Table;
 
 
 @Entity
+@Table(name="students")
 public class Student {
     
     @Id
--- src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
@@ -1,1 +1,20 @@
-// package com.example.demo.serviceimpl;
+package com.example.aiml.Serviceimpl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.aiml.entity.Student;
+import com.example.aiml.repository.StudentRepository;
+import com.example.aiml.service.StudentService;
+
+@Service
+public class StudentServiceImple implements StudentService{
+    @Autowired
+    StudentRepository studentRepository;
+    
+
+    public Student saveStudent(Student student) {
+        return studentRepository.save(student);
+    }
+    
+}
