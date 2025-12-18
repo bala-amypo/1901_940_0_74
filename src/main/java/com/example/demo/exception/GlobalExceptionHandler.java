d22b0093bac305143ed67e1463b8a8b6066af845
bala-amypo <bala@amypo.in>
2025-12-18 08:09:49 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -2,6 +2,9 @@ package com.example.demo.exception;
 @RestControllerAdvice
 public class GlobalExceptionHandler{
     @ExceptionHandler(ResourceNotFoundExpection.class){
-        
+        public ResponseEntity<>.(ex.getMessage(),HTTPStatus.NOT_FOUND){
+            return new 
+        }
     }
+
 }
