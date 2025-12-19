c5f8803895e799b699bae51abfbe4798f372aa66
bala-amypo <bala@amypo.in>
2025-12-19 04:45:00 +0000

    User commit

--- src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/serviceimpl/StudentServiceImple.java
@@ -21,4 +21,5 @@ public class StudentServiceImple implements StudentService {
         return studentRepository.findById(id)
         .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
     }
+    public Student updatedata(Long id,
 }
